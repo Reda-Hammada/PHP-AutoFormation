58eebd6d1bbd1b05dd23ebd224deef78389bf6bf
reda hammada <hammada.reda.dev@gmail.com>
2022-02-06 15:15:44 +0100

    object & classes added

--- dataTypes/Class&Object/classObject.php
+++ dataTypes/Class&Object/classObject.php
@@ -1,17 +1,24 @@
 <?php
-
+// public 
 class bankAccount{
-
+    // properties
     public $accountNumber;
     public $balance;
+    // methods
+    public function getBankAccountOwner(){
+         $accountOwner = "reda hammada";
+
+         return $accountOwner;
+    }
 }
 
 
 $account1 = new bankAccount();
 $account1 ->accountNumber = 1;
 $account1 ->balance = 5000;
+$account1 ->getBankAccountOwner();
 
-
+echo "the account owner name is : " . $account1 ->getBankAccountOwner() . "</br>";
 echo  "the account number is : " . $account1 ->accountNumber . "</br>";
 echo  "the account balance is : " . $account1 ->balance;
 

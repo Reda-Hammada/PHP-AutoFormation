a8bd98fedb99980278d641f8ca94d21da9078bf9
reda hammada <hammada.reda.dev@gmail.com>
2022-02-06 14:26:07 +0100

    array added

--- dataTypes/array/array.php
+++ dataTypes/array/array.php
@@ -1,5 +1,8 @@
 <?php
+// first way to declare an array
  $items = array('laptop','mobile','monitor');
- echo " the itmes are: " .  $items[0] . " " . $items[1] . "," . $items[2];
-
+ echo " the itmes are: " .  $items[0] . " " . $items[1] . "," . $items[2] . "</br>";
+// second way to declare an array
+$cars = ['volvo','BMW','Reneaut'];
+echo "the car is : " . $cars[1];
 ?>

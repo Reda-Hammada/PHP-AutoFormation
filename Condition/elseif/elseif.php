909c1e706ada40dd223364c0ffcdb9ea1cb91c01
reda hammada <hammada.reda.dev@gmail.com>
2022-02-08 21:50:26 +0100

    update

---
+++ Condition/elseif/elseif.php
@@ -0,0 +1,23 @@
+<?php
+
+$x = 10;
+$y = 100;
+
+if($x == $y){
+    $meassage = "x equals y";
+
+}
+
+elseif($x > $y){
+
+   $meassage= "x is greater than y";
+
+}
+
+elseif($x < $y){
+   $meassage = "y is greater than x";
+}
+
+echo $meassage;
+
+?>

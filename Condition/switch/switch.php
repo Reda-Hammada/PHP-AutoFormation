577f79e1827a0e48f8a02d4bb7e1c67a725b33a2
reda hammada <hammada.reda.dev@gmail.com>
2022-02-09 15:06:31 +0100

    update

---
+++ Condition/switch/switch.php
@@ -0,0 +1,23 @@
+<?php
+
+$role = 'admin';
+$message = '';
+
+switch ($role) {
+	case 'admin':
+		$message = 'Welcome, admin!';
+		break;
+	case 'editor':
+		$message = 'Welcome! You have some pending articles to edit';
+		break;
+	case 'author':
+		$message = 'Welcome! Do you want to publish the draft article?';
+		break;
+	case 'subscriber':
+		$message = 'Welcome! Check out some new articles.';
+		break;
+	default:
+		$message = 'You are not authorized to access this page';
+}
+
+echo $message;

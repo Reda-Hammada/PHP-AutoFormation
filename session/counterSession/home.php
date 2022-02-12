d98e369ac19c0a694ca2f8556e10484390a6464a
reda hammada <hammada.reda.dev@gmail.com>
2022-02-12 17:50:40 +0100

    update

--- session/counterSession/home.php
+++ session/counterSession/home.php
@@ -2,10 +2,9 @@
 
 require("c.php");
 
-echo "welcome" . " " . $_SESSION['username'] . "you have visited this page" . " " . $_SESSION['counter'] . " "  ."times"; 
+echo "welcome" . " " . $_SESSION['username'] . " " . "you have visited this page" . " " . $_SESSION['counter'] . " "  ."times"; 
 
 
-echo "<a href='session.php'>session</a>";
 
 
 ?>

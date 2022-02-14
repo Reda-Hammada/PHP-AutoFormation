70b6c51984f1a3a84a81d03ca8502213f3c66ed1
reda hammada <hammada.reda.dev@gmail.com>
2022-02-14 17:42:54 +0100

    update

--- dataTypes/array/arrayFunction/arrayFunction.php
+++ dataTypes/array/arrayFunction/arrayFunction.php
@@ -21,9 +21,27 @@ $colors = array(
 
 $colors +=  array("white" => "#fff");
 
-echo $colors['white'];
+echo $colors['white'] . "<br>";
 
 
 
+// array push allows us to add elements to the end of an array 
+$numbers = [1, 2, 3, 4];
+
+array_push($numbers, 5, 6, 7);
+print_r($numbers);
+
+// associative array 
+
+$roles = [
+
+    'admin' => 1,
+    'editor' => 2,
+    'approver' => 3
+];
+
+$roles['moderator'] = 4;
+
+print_r($roles);
 
 ?>

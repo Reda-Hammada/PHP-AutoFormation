2c06c5ec21f4529153079418b0cba1a5c4a92749
reda hammada <hammada.reda.dev@gmail.com>
2022-02-14 16:46:27 +0100

    multidimensional array added

--- dataTypes/array/array.php
+++ dataTypes/array/array.php
@@ -11,5 +11,32 @@ $age = array("Reda" => 21, "Omar" => 20, "Anass" => 21);
 
 echo "Reda is " . $age['Reda'] . " " . "years old";
 echo "Anass is " . $age['Anass'] . " ". "years old";
-echo "Omar is " . $age['Omar'] . " " . "years old"
+echo "Omar is " . $age['Omar'] . " " . "years old";
+
+// Multidimensional Arrays, each element in the main array can also be an array and also each element in the sub-array can be an array 
+
+$occupation = array(
+
+    "Reda" => array(
+
+        "occupation" => "developer",
+        "specialty" => "full stack web developer",
+        "experience" => "5 years"
+    ),
+
+    "Anass" => array(
+
+        "occupation" => "developer",
+        "specialty" => "front-end web developer",
+        "experience" => "5 years"
+
+    )
+
+
+
+    );
+
+    echo "Reda is a " . $occupation["Reda"] ["specialty"] . " " . ",and he has " . $occupation["Reda"] ["experience"] . " experience";
+
+
 ?>

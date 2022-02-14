c1347d95b9639b336dcdce15d3240e58cf238fc1
reda hammada <hammada.reda.dev@gmail.com>
2022-02-14 16:27:05 +0100

    associtive array added

--- dataTypes/array/array.php
+++ dataTypes/array/array.php
@@ -1,8 +1,15 @@
 <?php
 // first way to declare an array
  $items = array('laptop','mobile','monitor');
- echo " the itmes are: " .  $items[0] . " " . $items[1] . "," . $items[2] . "</br>";
+ echo " the itmes are: " .  $items[0] . " " . $items[1] . "," . $items[2] . "<br>";
 // second way to declare an array
 $cars = ['volvo','BMW','Reneaut'];
-echo "the car is : " . $cars[1];
+echo "the car is : " . $cars[1] . "<br>" ;
+
+// Associative Array , an array with strings as index
+$age = array("Reda" => 21, "Omar" => 20, "Anass" => 21);
+
+echo "Reda is " . $age['Reda'] . " " . "years old";
+echo "Anass is " . $age['Anass'] . " ". "years old";
+echo "Omar is " . $age['Omar'] . " " . "years old"
 ?>

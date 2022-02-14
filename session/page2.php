7b78b1e3689b858474f2fdfc2d05c88bca0db9b8
redahm <hammada.reda.dev@gmail.com>
2022-02-14 13:18:14 +0100

    update

--- session/page2.php
+++ session/page2.php
@@ -6,4 +6,4 @@
 session_start();
 
 echo 'Hello'. " " . $_SESSION['username'] . " " . "welcome again ! " . "</br>";
-echo '<a href="sessionDestroy.php">log out</a>';
+echo '<a href="sessionDestroy.php"> click to log out</a>';

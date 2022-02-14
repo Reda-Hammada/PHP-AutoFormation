09b2024088becda6a26d327a2eb0dfe7b4b949eb
reda hammada <hammada.reda.dev@gmail.com>
2022-02-14 17:15:00 +0100

    array_unshift added

---
+++ dataTypes/array/arrayFunction/arrayFunction.php
@@ -0,0 +1,10 @@
+<?php
+// array_unshift add elements to the beginning of the array 
+$admin = array('reda', 'ali', 'ahmed');
+array_unshift($admin, 'omar');
+
+print_r($admin);
+echo $admin[0];
+
+
+?>

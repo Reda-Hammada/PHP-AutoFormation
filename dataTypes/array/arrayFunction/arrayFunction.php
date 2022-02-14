1a828696f3440f9dfe45fa0acc73a6d6cf9a4f2e
reda hammada <hammada.reda.dev@gmail.com>
2022-02-14 17:25:20 +0100

    array_unshift and prepending associative array

--- dataTypes/array/arrayFunction/arrayFunction.php
+++ dataTypes/array/arrayFunction/arrayFunction.php
@@ -4,7 +4,26 @@ $admin = array('reda', 'ali', 'ahmed');
 array_unshift($admin, 'omar');
 
 print_r($admin);
-echo $admin[0];
+echo $admin[0] . "<br>";
+
+
+// prepend associative array 
+
+$colors = array(
+
+
+        'red' => '#ff000',
+        'green' => '#00ff00',
+        'blue' => '#0000ff',
+
+);
+
+
+$colors +=  array("white" => "#fff");
+
+echo $colors['white'];
+
+
 
 
 ?>

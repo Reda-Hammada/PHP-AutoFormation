4a5e7bc639cb22513fe6e825db325549b0b78625
redahm <hammada.reda.dev@gmail.com>
2022-02-15 13:47:30 +0100

    update

---
+++ oop/constructor/constructor.php
@@ -0,0 +1,22 @@
+<?php
+
+class person{
+
+   private $name;
+   private $age;
+
+   function __construct($name, $age){
+
+   echo $name   . $age ;
+
+    
+   }
+
+}
+
+
+$person1  = new person('Reda', 21);
+
+
+
+?>

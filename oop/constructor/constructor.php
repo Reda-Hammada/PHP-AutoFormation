24f279ae80778cbf8907b41165da0dd34fff347a
reda hammada <hammada.reda.dev@gmail.com>
2022-02-15 18:05:48 +0100

    inheritance finished

--- oop/constructor/constructor.php
+++ oop/constructor/constructor.php
@@ -1,7 +1,7 @@
 <?php
 
 class person{
-
+   
    private $name;
    private $age;
 
--- oop/inheritance/inheritance.php
+++ oop/inheritance/inheritance.php
@@ -1,7 +1,7 @@
 <?php
 class bankAccount {
 
-   private $balance;
+   private $balance; 
    private $amount;
 
    public function  getBalance(){
@@ -28,10 +28,22 @@ class bankAccount {
 
   
   public function deposit(){
-
+   
     if($amount > 0){
 
         $balance += $amount; 
+
+        return $balance;
+    }
+  }
+
+  public function withdraw(){
+
+    if($amount > 0){
+
+        $balance -= $amount;
+
+        return $balance;
     }
   }
 
@@ -40,8 +52,15 @@ class bankAccount {
 
 }
 
+class createAccount extends bankAccount {
 
 
+}
 
 
+$account1 = new createAccount();
+$account1 -> balance = 6000 ;
+$account1 -> amount = 1200;
+
+echo $account1-> balance + $account1-> amount . " USD";
 ?>

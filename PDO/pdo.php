03e4211963d5506d5a61a4d792888bfaffd0c002
reda hammada <hammada.reda.dev@gmail.com>
2022-04-27 01:23:38 +0000

    pdo query

--- PDO/pdo.php
+++ PDO/pdo.php
@@ -1,17 +1,31 @@
 <?php 
 
-$host = 'locathost';
+$host = 'localhost';
 $user = 'Reda';
 $password = '123456';
 $dbName = 'liveCodingCart';
 
 // set DSN
 
-$dsn = 'mysql:host='. $host . ';dbName=' .$dbName;
+$dsn = 'mysql:host=' . $host .  ';dbname=' . $dbName;
 
 //Create a PDO instance
 
 $pdo = new PDO($dsn, $user, $password);
+
+//PDO query
+
+$stmt = $pdo->query('SELECT * FROM liveCodingProducts ');
+
+$row = null; 
+
+if($row == null):
+
+    $row =  $stmt->fetch(PDO::FETCH_OBJ);
+    
+    echo $row->price;
+    
+endif;
 
 
 ?>

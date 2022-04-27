8c33f15283cd8b4de0f4142092eb21f37c67f074
reda hammada <hammada.reda.dev@gmail.com>
2022-04-27 02:10:17 +0000

    prepared statements added

--- PDO/pdo.php
+++ PDO/pdo.php
@@ -23,9 +23,33 @@ if($row == null):
 
     $row =  $stmt->fetch(PDO::FETCH_OBJ);
     
-    echo $row->price;
-    
+    echo $row->price . "<br>";
+
 endif;
+
+//prepared statements(prepare & execute)
+
+// positional statement 
+
+$sql = 'SELECT * FROM  liveCodingProducts WHERE productName = ? ';
+$stmt = $pdo->prepare($sql);
+$product = 'product 4';
+$stmt->execute([$product]);
+$product =  $stmt->fetch(PDO::FETCH_OBJ);
+echo var_dump($product) . "<br>";
+
+//named statment 
+
+$sql  = 'SELECT * FROM liveCodingProducts WHERE productName = :product4';
+$stmt = $pdo->prepare($sql);
+$product = 'product 4';
+$stmt->execute(['product4' => $product]);
+$fetch = $stmt->fetch(PDO::FETCH_ASSOC);
+echo $fetch['price'];
+
+
+
+
 
 
 ?>

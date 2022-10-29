68033a6fe8b69505a24b228ab953682c741afa9b
Reda Hammada <hammada.reda.dev@gmail.com>
2022-10-29 18:25:55 +0200

    multi interfaces implementation

--- oop/interface/interface.php
+++ oop/interface/interface.php
@@ -25,11 +25,78 @@
 
     public function methodName($name){
 
-        echo $this->name;
+        echo $this->name . '<br>';
 
     }
   }
 
   $class = new MyClass();
 
+
+    interface Readable
+    {
+        public function read();
+    }
+
+    interface Document extends Readable
+    {
+        public function getContents();
+    }
+
+
+    // multi interfaces 
+
+    interface name {
+
+        public function printName();
+
+    }
+
+    interface age {
+
+        public function printAge();
+
+    }
+
+    interface occupation {
+
+        public function printOccupation();
+    }
+
+    class Person implements name,age,occupation {
+
+        private $name = 'Reda Hammada';
+        private $age = '22';
+        private $occupation = 'PHP developer';
+
+        public function __construct ()
+        {
+
+            $this->printName();
+            $this->printAge();
+            $this->printOccupation();
+
+        }
+
+
+        public function printName ()
+        {
+                echo $this->name . '<br>';
+        }
+
+        public function printAge  ()
+        {
+            echo $this->age . '<br>';
+
+        }
+
+        public function printOccupation ()
+        {
+            echo $this->occupation . '<br>';
+
+        }
+    }
+
+
+    $person = new Person();
 ?>

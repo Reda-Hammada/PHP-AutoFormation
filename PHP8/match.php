ab995ba1ae7f8e29d2a72a63cf9892ad8074b765
Reda Hammada <hammada.reda.dev@gmail.com>
2023-01-29 16:01:57 +0100

    update

--- PHP8/match.php
+++ PHP8/match.php
@@ -3,6 +3,22 @@
  
  $language =  new German();
 
+ switch(get_class($language)){
+    case 'German':
+        $type = 'Hallo';
+        break;
+    case 'Egnlish ':
+        $type = 'Hello';
+        break;
+    case 'French':
+        $type = 'Bonjour';
+        break;
+    case 'Spanish':
+        $type = 'Holla';
+        break;
+                   
+ }
+
 $type = match(get_class($language)){
     
     'German' => 'Hallo',
---
+++ PHP8/objclass.php
@@ -0,0 +1,32 @@
+<?php 
+class german {
+    public function __construct()
+    {
+        echo 'Guten Tag, Ich heiße Reda';
+    }
+}
+
+ $language = new german();
+ $language::class;
+ /* as an alternative for get_class() 
+    we can use $object::class to get the class name*/
+
+
+get_class($language) === $language::class;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
